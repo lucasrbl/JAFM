a8ec8ee5ef5551a765b3b6f5d18868204bfff41c
Diogo Almeida <diogo.meida@gmail.com>
2023-11-17 16:14:28 -0300

    reports forms

--- App.js
+++ App.js
@@ -6,12 +6,13 @@ import { CustomCarousel } from './components/screens/Carousel/Carousel';
 import { MyStack } from './components/routes/AppNavigator';
 import { MyTab } from './components/routes/RootNavigator';
 import { Reports } from './components/screens/Reports/Reports';
+import { ReportsForms } from './components/screens/ReportsForms/ReportsForms';
 import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Reports/>
+      <ReportsForms/>
     </NavigationContainer>
   );
 }
