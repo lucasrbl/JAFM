367dd660bd84068c79d33e3699e8af059479d05a
ApMatheus <matheusaparecido101@gmail.com>
2023-12-07 19:52:26 -0300

    fix: adjustments on the Welcome page

--- components/screens/Welcome/Welcome.tsx
+++ components/screens/Welcome/Welcome.tsx
@@ -15,29 +15,40 @@ export const Welcome: React.FC = () => {
 
         content: {
             flex: 1,
-            justifyContent: "flex-end"
+            display: "flex",
+            justifyContent: "flex-end",
+            zIndex: 2,
+            position: "absolute",
+            bottom: 0,
+            width: "100%",
         },
 
         textContainer: {
             alignItems: "center",
             gap: 3,
-            marginBottom: 30
+
         },
 
         buttonContainer: {
             alignItems: "center",
             gap: 5,
-            marginBottom: 30
         },
 
         image: {
+            height: "100%",
+            width: "100%",
             top: 80,
-            left: 5.5
+            resizeMode: 'contain',
         },
 
         linearGradient: {
             width: "100%",
             height: "100%"
+        },
+
+        linearGradientContent:{
+            flex:1,
+            gap:16
         },
 
         button: {
@@ -49,24 +60,29 @@ export const Welcome: React.FC = () => {
 
 
     return (
-       <LinearGradient colors={["#F65151", "rgba(0, 0, 0, .95)"]} style={styles.linearGradient}> 
-        <View style={styles.wrapper}>
-            <Image source={require("../../../assets/tela-inicial.png")} style={styles.image} resizeMode="cover" />
-           <View style={styles.content}> 
-            <View style={styles.textContainer}>
-                <CustomText text="Bem-vindo ao JAFM!" color="#FFFFFF" size={36} fontWeight="bold" />
-                <CustomText text="Programa feito para o jovem aprendiz" color="#FFFFFF" fontWeight="bold" />
-            </View>
+        <LinearGradient colors={["#F65151", "rgba(0, 0, 0, .95)"]} style={styles.linearGradient}>
+            <View style={styles.wrapper}>
+                <Image source={require("../../../assets/tela-inicial.png")} style={styles.image} resizeMode="cover" />
+                <View style={styles.content}>
+                    <LinearGradient
+                        colors={["transparent", "rgba(0, 0, 0, .95)"]}
+                        style={styles.linearGradientContent}>
 
-            
-                <View style={styles.buttonContainer}>
-                    <LinearGradient colors={["#F65151", "#962727"]} style={styles.button}>
-                        <CustomButton title="Criar conta" width={300} padding={16} size={16} onPress={() => navigation.navigate("Register")} />
+                        <View style={styles.textContainer}>
+                            <CustomText text="Bem-vindo ao JAFM!" color="#FFFFFF" size={36} fontWeight="bold" />
+                            <CustomText text="Programa feito para o jovem aprendiz" color="#FFFFFF" fontWeight="bold" />
+                        </View>
+
+
+                        <View style={styles.buttonContainer}>
+                            <LinearGradient colors={["#F65151", "#962727"]} style={styles.button}>
+                                <CustomButton title="Criar conta" width={300} padding={16} size={16} onPress={() => navigation.navigate("Register")} />
+                            </LinearGradient>
+                            <CustomButton title="Entrar" color="#FFFFFF" width={300} padding={16} radius={12} size={16} onPress={() => navigation.navigate("Login")} />
+                        </View>
                     </LinearGradient>
-                    <CustomButton title="Entrar" color="#FFFFFF" width={300} padding={16} radius={12} size={16} onPress={() => navigation.navigate("Login")}/>
                 </View>
             </View>
-        </View>
         </LinearGradient>
     )
 }
